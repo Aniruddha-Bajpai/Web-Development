2a6074a920b2fa9827e82ccd118080d176aef56c
Aniruddha Bajpai <bajpaianirudh03@gmail.com>
2022-12-04 15:34:16 +0530

    added more projects

---
+++ JavaScript/basic.js
@@ -0,0 +1,6 @@
+// var myname = "Aniruddh";
+// myname = "dhoni";
+// alert("My name is " + myname);
+
+var x = NULL;
+alert("VALUE = " + x);
---
+++ JavaScript/hoisting.js
@@ -0,0 +1,9 @@
+// getName();
+console.log(getname);
+
+// var x = 7;
+var getname = function getName() {
+  console.log("Namaster JavaScript");
+};
+// getName();
+// console.log(x);
